10dd05926b3fdc6dac51f4096559e77d9539fed4
Dustin Schau <dustinschau@gmail.com>
2019-01-25 15:31:06 -0600

    chore: tweak slides

--- apps/slides/deck.js
+++ apps/slides/deck.js
@@ -1,23 +1,23 @@
-import intro from './topics/intro.mdx'
-import gatsbyOverview from './topics/gatsby-overview.mdx'
-import newFeatures from './topics/new-features.mdx'
-import demo from './topics/demo.mdx'
-import qAndA from './topics/q-and-a.mdx'
-import conclusion from './topics/conclusion.mdx'
+import intro from './topics/intro.mdx';
+import gatsbyOverview from './topics/gatsby-overview.mdx';
+import newFeatures from './topics/new-features.mdx';
+import demo from './topics/demo.mdx';
+import qAndA from './topics/q-and-a.mdx';
+import conclusion from './topics/conclusion.mdx';
 
-// import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css'
-// import 'typeface-space-mono'
-// import 'typeface-spectral'
+import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css';
+import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css';
+import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css';
+import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css';
+import 'typeface-space-mono';
+import 'typeface-spectral';
 
-export { default as theme } from 'gatsby-mdx-theme'
+export { default as theme } from 'gatsby-mdx-theme';
 export default [
   ...intro,
   ...gatsbyOverview,
   ...newFeatures,
   ...demo,
   ...qAndA,
-  ...conclusion
-]
+  ...conclusion,
+];
