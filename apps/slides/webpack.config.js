577b98afa0ff3ac3a31fb5a04476efb4e68b3b20
Dustin Schau <dustinschau@gmail.com>
2019-01-28 16:24:10 -0600

    feat: pass env vars to mdx-deck

--- apps/slides/webpack.config.js
+++ apps/slides/webpack.config.js
@@ -1,14 +1,30 @@
+const webpack = require('webpack');
+
 module.exports = {
+  plugins: [
+    new webpack.DefinePlugin(
+      Object.keys(process.env).reduce((merged, key) => {
+        const mergedKey = `process.env.${key}`;
+        let value = process.env[key];
+        if (value === 'true' || value === 'false') {
+          merged[mergedKey] = JSON.parse(value);
+        } else {
+          merged[mergedKey] = JSON.stringify(process.env[key]);
+        }
+        return merged;
+      }, {})
+    ),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)/,
-        use: ['file-loader']
-      }
-    ]
-  }
-}
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
